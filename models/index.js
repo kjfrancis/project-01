2f3c2742093fb152392c4ff5f4f923b056fd2b22
kjfrancis <kef2010@gmail.com>
2016-04-09 17:27:54 -0700

    working on index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/primary-demo");
-
+mongoose.connect("mongodb://localhost/secondary-demo");
 
 var Primary = require('./primary');
+var Secondary = require('./secondary');
+
 
 module.exports.Primary = Primary;
+module.exports.Secondary = Secondary;
